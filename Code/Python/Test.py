572c385f36ba3b026fa1c43d744a2204324eb76a
ViktoriaWasTaken <Pwhodge03@gmail.com>
2022-11-18 20:45:59 -0600

    test 1

---
+++ Code/Python/Test.py
@@ -0,0 +1,20 @@
+name = "Tori"
+
+age = 19
+
+print(name)
+print(age)
+print(type(name))
+print(type(age))
+#math and a string cant be on the same print cmd
+math = 9 + 7 * 8 / 2 - 7
+#\n starts a new line
+name = input("welcome what is your name!?\n")
+
+print("Nice to meet you " + name)
+
+options = "Run, Jump, Bike, or Swim?\n"
+
+choice = input("Would you like to " + options)
+
+print("alright lets " + choice)
